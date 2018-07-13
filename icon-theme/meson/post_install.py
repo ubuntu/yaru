a90139e1eefcca95963aecddf6ac4046f6c6d6b5
Didier Roche <didrocks@ubuntu.com>
2018-07-13 17:43:32 +0200

    Build the entire theme from a main meson file
    Rename all subdirectory to match the same name.
    Ensure that all generated files are dynamically matching the project
    name.

--- icon-theme/meson/post_install.py
+++ icon-theme/meson/post_install.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
+from contextlib import suppress
 from os import environ, path
 from subprocess import call
+import sys
+
+project_name = 'Suru'
+with suppress(IndexError):
+    project_name = sys.argv[1]
 
 if not environ.get('DESTDIR', ''):
     PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr')
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(PREFIX, 'share/icons/Suru')])
+    call(['gtk-update-icon-cache', '-qtf', path.join(PREFIX, 'share/icons', project_name)])
