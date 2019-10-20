9cbd6ab1460d36593d2a823811b4578e7c66103c
Carlo Lobrano <c.lobrano@gmail.com>
2019-10-20 11:50:38 +0200

    shell: install both variants in alt themes dir
    Both light and dark gnome shell variants are installed under the alt
    themes dir (/usr/share/themes/<theme-name>) and the default one (currently
    the light one) is symlinked under /usr/share/gnome-shell/theme/<theme-name>.
    
    This implementation is easier to understand than install the two variants in
    two different places.
    
    NOTE: consider to refactor variants' meson build scripts because now they are
    exactly the same.

--- gnome-shell/src/install-shell.sh
+++ gnome-shell/src/install-shell.sh
@@ -4,4 +4,4 @@
 project_name="$1"
 prefix="${MESON_INSTALL_DESTDIR_PREFIX}/share"
 
-ln -sf "${prefix}/gnome-shell/theme/${project_name}" "${prefix}/themes/${project_name}/gnome-shell"
+ln -sf "${prefix}/themes/${project_name}/gnome-shell" "${prefix}/gnome-shell/theme/${project_name}"
