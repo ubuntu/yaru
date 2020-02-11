f26048dd05c02b20586feec2c1432c0339d9163a
Carlo Lobrano <c.lobrano@gmail.com>
2020-02-11 18:35:27 +0100

    comment out install script for snap store complain about symlinks
    Switching theme will not work, but the default one won't be affected

--- gnome-shell/src/install-shell.sh
+++ gnome-shell/src/install-shell.sh
@@ -5,5 +5,6 @@ project_name="$1"
 destdir_prefix="${MESON_INSTALL_DESTDIR_PREFIX}/share"
 install_prefix="${MESON_INSTALL_PREFIX}/share"
 
-mkdir -p "${destdir_prefix}/themes/${project_name}"
-ln -sf "${install_prefix}/gnome-shell/theme/${project_name}" "${destdir_prefix}/themes/${project_name}/gnome-shell"
+# FIXME tentative fix for "The store was unable to accept this snap: package contains external symlinks"
+#mkdir -p "${destdir_prefix}/themes/${project_name}"
+#ln -sf "${install_prefix}/gnome-shell/theme/${project_name}" "${destdir_prefix}/themes/${project_name}/gnome-shell"
