de293b932da2281a34ee7ecaf2ca9d130c12e49f
Carlo Lobrano <c.lobrano@gmail.com>
2020-03-13 23:25:36 +0100

    use relative path for symlink
    review-tools for snap build forbid absolute link, then we use use
    relative path respect the destdir_prefix.

--- gnome-shell/src/install-shell.sh
+++ gnome-shell/src/install-shell.sh
@@ -5,5 +5,15 @@ project_name="$1"
 destdir_prefix="${MESON_INSTALL_DESTDIR_PREFIX}/share"
 install_prefix="${MESON_INSTALL_PREFIX}/share"
 
-mkdir -p "${destdir_prefix}/themes/${project_name}"
-ln -sf "${install_prefix}/gnome-shell/theme/${project_name}" "${destdir_prefix}/themes/${project_name}/gnome-shell"
+echo meson install destdir prefix ${MESON_INSTALL_DESTDIR_PREFIX}
+echo meson install prefix ${MESON_INSTALL_PREFIX}
+
+set -x
+dest_dir="${destdir_prefix}/themes/${project_name}"
+mkdir -p "${dest_dir}"
+
+# Use relative path for symlink, since the review-tools
+# check do not allow absolute links
+rel_install_prefix=$(realpath --relative-to="${dest_dir}" "${install_prefix}")
+
+ln -sf "${rel_install_prefix}/gnome-shell/theme/${project_name}" "${dest_dir}/gnome-shell"
