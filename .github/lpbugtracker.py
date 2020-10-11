20339e4128796b69f2df1a059c36a170288d429b
Carlo Lobrano <c.lobrano@gmail.com>
2020-10-11 15:40:25 +0200

    Setup GitHub action to track LP issues
    This is an initial deploy for the resources needed to automatically
    track LP issues on Yaru-theme package.
    
    This commit contains:
    - a python script that uses launchpadlib to get a list of yaru-theme
      bugs on Launchpad.
    - a new GitHub action yaml file to configure the automation.
    - the first copy of the json file containing the LP issues.
    
    The automation is initially supposed to:
    1. download the list of LP issues for Yaru and store them in a json file.
    2. compare the above list with the one under launchpad/yaru_lp_bugs.json.
    3. if the two files differs, the new one is stored and a PR is created.
    
    A maintainer is then supposed to take the list, which contains ID, Title,
    and web link, and manually check the issues, eventually adding or removing
    bugs.
    
    The python script is able to extract which bugs are actually new, and it is
    possible that a future configuration will create the bugs automatically,
    but this is left for future development, once this first step is working fine.

---
+++ .github/lpbugtracker.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vi: set ft=python :
+"""
+LP Bug Tracker uses launchpadlib to get the Yaru-theme bugs.
+The first time, the full list is saved in a json file.
+The next times, the newly get list of LP yaru-theme bugs are compared with the ones
+stored in the json file to show if any new issue was created since last check.
+"""
+
+import os
+import json
+import argparse
+from launchpadlib.launchpad import Launchpad
+
+HOME = os.path.expanduser("~")
+CACHEDIR = os.path.join(HOME, ".launchpadlib", "cache")
+YARU_LP_BUGS_FILE = "yaru_lp_bugs.json"
+YARU_LP_BUGS_DIR = "launchpad"
+
+
+def get_lp_bugs(dest):
+    """ Get LP active bugs and save in dest as json file """
+    bugs = get_yaru_launchpad_bugs()
+    if not dest:
+        dest = YARU_LP_BUGS_FILE
+    elif os.path.isdir(dest):
+        dest = os.path.join(dest, YARU_LP_BUGS_FILE)
+
+    save_bug_list(bugs, dest)
+
+
+def diff_bugs(source, destination):
+    """Reads a list of bugs from the {source} json file,
+    compares them with the ones stored in Yaru repo and
+    save the list of the untracked bugs as json file in
+    {destination}."""
+
+    bugs_ref = get_bug_list(source)
+    if not bugs_ref:
+        print("[!] cannot find bugs in %s" % source)
+        return
+
+    bugs_stored = get_bug_list(os.path.join(YARU_LP_BUGS_DIR, YARU_LP_BUGS_FILE))
+    bugs_diff = {}
+    for id in bugs_ref:
+        if bugs_stored.get(id, None) is None:
+            print("Untracked LP bug: %s - %s" % (id, bugs_ref[id]["title"]))
+            bugs_diff[id] = bugs_ref[id]
+
+    if len(bugs_diff):
+        save_bug_list(bugs_diff, destination)
+
+
+def get_yaru_launchpad_bugs():
+    """Get a list of active bugs from Launchpad"""
+
+    lp = Launchpad.login_anonymously(
+        "Yaru LP bug checker", "production", CACHEDIR, version="devel"
+    )
+
+    ubuntu = lp.distributions["ubuntu"]
+    archive = ubuntu.main_archive
+
+    packages = archive.getPublishedSources(source_name="yaru")
+    package = ubuntu.getSourcePackage(name=packages[0].source_package_name)
+
+    bug_tasks = package.searchTasks()
+    bugs = {}
+
+    for task in bug_tasks:
+        id = str(task.bug.id)
+        title = task.title.split(": ")[1]
+        link = "https://bugs.launchpad.net/ubuntu/+source/yaru-theme/+bug/" + str(id)
+        bugs[id] = {"title": title, "link": link}
+
+    return bugs
+
+
+def save_bug_list(bugs, destination):
+    with open(destination, "w") as f:
+        f.write(json.dumps(bugs, indent=2))
+
+
+def get_bug_list(source):
+    try:
+        with open(source, "r") as f:
+            bugs = json.loads(f.read())
+    except FileNotFoundError:
+        bugs = {}
+    return bugs
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--get-lp-bugs", action="store_true", help="get Yaru active bugs on Launchpad"
+    )
+    parser.add_argument(
+        "--diff-bugs",
+        action="store_true",
+        help="compare bug lists and save the new bugs.",
+    )
+    parser.add_argument("--destination", help="json file path to save a bug list")
+    parser.add_argument("--source", help="json file path containing a bug list to read")
+
+    args = parser.parse_args()
+
+    if args.get_lp_bugs:
+        get_lp_bugs(args.destination)
+
+    if args.diff_bugs:
+        diff_bugs(args.source, args.destination)
+
+    # lp_bugs = get_yaru_launchpad_bugs()
+    # already_stored_bugs = get_bug_list(YARU_LP_BUGS_FILE)
+
+    # lp_bugs_new = []
+    # for id in lp_bugs:
+    # if already_stored_bugs.get(id, None) is None:
+    # lp_bugs_new.append(id)
+    # print(lp_bugs_new)
+    # if len(lp_bugs_new):
+    # save_bug_list(lp_bugs, YARU_LP_BUGS_FILE)
+
+    # save_bug_list(lp_bugs)
