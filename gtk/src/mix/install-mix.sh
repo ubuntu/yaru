218fc8ebf903b7c51b8fd86310918e17c4881c86
Muqtadir <muqtadir@pop-os.localdomain>
2021-07-13 22:44:03 +0530

    Update mix symlinks
    reason:
    - do not present yaru-mix themes under GNOME-tweaks to avoid confusion.
    
    action:
    - remove gtk-3.0 symlinks under yaru-mix

--- gtk/src/mix/install-mix.sh
+++ gtk/src/mix/install-mix.sh
@@ -9,11 +9,11 @@ srcdir="${MESON_SOURCE_ROOT}/mix"
 datadir="${MESON_INSTALL_DESTDIR_PREFIX}/$1"
 
 install -m755 -d "${datadir}/themes/Yaru-mix-dark"
-for file in index.theme gtk-2.0 gtk-3.0 gtk-3.20; do
+for file in index.theme gtk-2.0 gtk-3.20; do
     ln -sfn "../Yaru/${file}" "${datadir}/themes/Yaru-mix-dark/${file}"
 done
 install -m755 -d "${datadir}/themes/Yaru-mix"
-for file in index.theme gtk-2.0 gtk-3.0 gtk-3.20; do
+for file in index.theme gtk-2.0 gtk-3.20; do
     ln -sfn "../Yaru-dark/${file}" "${datadir}/themes/Yaru-mix/${file}"
 done
 
