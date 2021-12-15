c21dc59cf35cfc1397f6b1915db38cbff5e23b7d
Paul Kepinski <paul.kepinski@orange.fr>
2021-12-15 12:17:39 +0100

    Render Gtk2 dark assets

--- gtk/src/dark/gtk-2.0/render-assets-external.sh
+++ gtk/src/dark/gtk-2.0/render-assets-external.sh
@@ -3,11 +3,10 @@
 INKSCAPE="inkscape"
 OPTIPNG="optipng"
 
-SRC_FILE="assets.svg"
+SRC_FILE="assets-external.svg"
 ASSETS_DIR="assets"
-INDEX="assets.txt"
+INDEX="assets-external.txt"
 
-rm assets/*.png
 for i in `cat $INDEX`
 do 
 if [ -f $ASSETS_DIR/$i.png ]; then
@@ -17,7 +16,8 @@ else
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              -o $ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+              --export-background-opacity=0 \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
     && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
 fi
 done
