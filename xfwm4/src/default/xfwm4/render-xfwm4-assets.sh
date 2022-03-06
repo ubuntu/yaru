04c638fff6b5ab7adb0e5eff13d00b838210d08f
JakeIsMeh <jakeismeh1@gmail.com>
2022-03-06 15:36:49 +0800

    Modify scripts to render HiDPi assets

--- xfwm4/src/default/xfwm4/render-xfwm4-assets.sh
+++ xfwm4/src/default/xfwm4/render-xfwm4-assets.sh
@@ -5,6 +5,8 @@ OPTIPNG="optipng"
 
 SRC_FILE="assets.svg"
 ASSETS_DIR="assets"
+HDPI_ASSETS_DIR="assets-hdpi"
+XHDPI_ASSETS_DIR="assets-xhdpi"
 
 INDEX="assets.txt"
 
@@ -14,9 +16,13 @@ has_command() {
 }
 
 mkdir -p $ASSETS_DIR
+mkdir -p $HDPI_ASSETS_DIR
+mkdir -p $XHDPI_ASSETS_DIR
 
 for i in `cat $INDEX`
 do
+
+# Normal
 if [ -f $ASSETS_DIR/$i.png ]; then
     echo $ASSETS_DIR/$i.png exists.
 else
@@ -28,5 +34,51 @@ else
               --export-filename=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null
     $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
 fi
+
+# HDPI
+if [ -f $HDPI_ASSETS_DIR/$i.png ]; then
+    echo $HDPI_ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $HDPI_ASSETS_DIR/$i.png
+
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-dpi=144 \
+              --export-filename=$HDPI_ASSETS_DIR/$i.png $SRC_FILE >/dev/null
+    $OPTIPNG -o7 --quiet $HDPI_ASSETS_DIR/$i.png
+fi
+
+# XHDPI
+if [ -f $XHDPI_ASSETS_DIR/$i.png ]; then
+    echo $XHDPI_ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $XHDPI_ASSETS_DIR/$i.png
+
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-dpi=192 \
+              --export-filename=$XHDPI_ASSETS_DIR/$i.png $SRC_FILE >/dev/null
+    $OPTIPNG -o7 --quiet $XHDPI_ASSETS_DIR/$i.png
+fi
 done
+
+# Symlink Titlebar Assets
+for num in {2..5}
+do
+  # Normal
+  ln -srfv $ASSETS_DIR/title-1-active.png $ASSETS_DIR/title-$num-active.png
+  ln -srfv $ASSETS_DIR/title-1-inactive.png $ASSETS_DIR/title-$num-inactive.png
+
+  # HDPI
+  ln -srfv $HDPI_ASSETS_DIR/title-1-active.png $HDPI_ASSETS_DIR/title-$num-active.png
+  ln -srfv $HDPI_ASSETS_DIR/title-1-inactive.png $HDPI_ASSETS_DIR/title-$num-inactive.png
+
+  # XHDPI
+  ln -srfv $XHDPI_ASSETS_DIR/title-1-active.png $XHDPI_ASSETS_DIR/title-$num-active.png
+  ln -srfv $XHDPI_ASSETS_DIR/title-1-inactive.png $XHDPI_ASSETS_DIR/title-$num-inactive.png
+done
+
+
 exit 0
