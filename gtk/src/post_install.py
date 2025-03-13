14fe53b071a4165b4a83a003525108e25b72812c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-03-13 13:54:14 +0100

    gtk: Use meson native features to rename configured files
    While this is not possible for custom targets, we can avoid use
    post-install script to rename configured files

--- gtk/src/post_install.py
+++ gtk/src/post_install.py
@@ -21,13 +21,6 @@ for f in flavours:
 
     flavour_dir = path.join(themes_dir, flavour_name)
 
-    # rename index.theme
-    theme_index_name = flavour_name + "-index.theme"
-    theme_index_src = path.join(flavour_dir, theme_index_name)
-    if path.exists(theme_index_src):
-        theme_index_dst = path.join(flavour_dir, 'index.theme')
-        move(theme_index_src, theme_index_dst)
-
     for gtkver in ['3.0', '4.0']:
         gtk_dir = path.join(flavour_dir, 'gtk-' + gtkver)
 
@@ -37,11 +30,3 @@ for f in flavours:
         if path.exists(theme_gresource_src):
             theme_gresource_dst = path.join(gtk_dir, 'gtk.gresource')
             move(theme_gresource_src, theme_gresource_dst)
-
-        # rename css
-        for variant in ['', '-dark']:
-            theme_gtk_css = "{flavour}-gtk{variant}-{ver}.css".format(flavour=flavour_name, ver=gtkver, variant=variant)
-            theme_gtk_css_src = path.join(gtk_dir, theme_gtk_css)
-            if path.exists(theme_gtk_css_src):
-                theme_gtk_css_dst = path.join(gtk_dir, 'gtk{variant}.css'.format(variant=variant))
-                move(theme_gtk_css_src, theme_gtk_css_dst)
