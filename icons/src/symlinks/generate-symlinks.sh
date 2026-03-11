5cbe44e6bf7053922588a649cfa452ce3078ef3d
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-03-11 11:52:59 +0100

    generate-symlinks: Quote variables and cleanup code

--- icons/src/symlinks/generate-symlinks.sh
+++ icons/src/symlinks/generate-symlinks.sh
@@ -36,56 +36,56 @@
 # CLInt GENERATED_CODE: start
 
 # No-arguments is not allowed
-[ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' $0 && exit 1
+[ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' "$0" && exit 1
 
 # Converting long-options into short ones
 for arg in "$@"; do
-	shift
-	case "$arg" in
-		"--all") set -- "$@" "-a";;
-		"--match") set -- "$@" "-m";;
-		"--variant") set -- "$@" "-t";;
-		"--verbose") set -- "$@" "-v";;
-		"--dry-run") set -- "$@" "-n";;
-		*) set -- "$@" "$arg"
-	esac
+    shift
+    case "$arg" in
+        "--all") set -- "$@" "-a";;
+        "--match") set -- "$@" "-m";;
+        "--variant") set -- "$@" "-t";;
+        "--verbose") set -- "$@" "-v";;
+        "--dry-run") set -- "$@" "-n";;
+        *) set -- "$@" "$arg"
+    esac
 done
 
 function print_illegal() {
-	echo Unexpected flag in command line \"$@\"
+    echo "Unexpected flag in command line '$*'"
 }
 
 # Parsing flags and arguments
 while getopts 'hanvm:t:' OPT; do
-	case $OPT in
-		h) sed -ne 's/^## \(.*\)/\1/p' $0
-			exit 1 ;;
-		a) _all=1 ;;
-		n) _dry_run=1 ;;
-		v) _verbose=1 ;;
-		m) _match=$OPTARG ;;
-		t) _variant=$OPTARG ;;
-		\?) print_illegal $@ >&2;
-			echo "---"
-			sed -ne 's/^## \(.*\)/\1/p' $0
-			exit 1
-			;;
-	esac
+    case $OPT in
+        h) sed -ne 's/^## \(.*\)/\1/p' "$0"
+            exit 1 ;;
+        a) _all=1 ;;
+        n) _dry_run=1 ;;
+        v) _verbose=1 ;;
+        m) _match=$OPTARG ;;
+        t) _variant=$OPTARG ;;
+        \?) print_illegal "$@" >&2;
+            echo "---"
+            sed -ne 's/^## \(.*\)/\1/p' "$0"
+            exit 1
+            ;;
+    esac
 done
 # CLInt GENERATED_CODE: end
 
-[ ! -z $_match ] && needle=$_match || needle=''
+[ -n "$_match" ] && needle="$_match" || needle=''
 
 function dlog() {
-	[ ! -z $_verbose ] && echo $*
+    [ -n "$_verbose" ] && echo "$@"
 }
 
 if [ "${BASH_VERSION%%.*}" -lt 4 ]; then
-	echo "Too old bash version ${BASH_VERSION}!";
-	exit 1
+    echo "Too old bash version ${BASH_VERSION}!";
+    exit 1
 fi
 
-DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1 ; pwd -P )
 
 # Icon sizes, contexts and variants
 CONTEXTS=(
@@ -119,93 +119,101 @@ SIZES=(
 OPTIONAL_SIZES=("8x8" "8x8@2x" "22x22")
 VARIANTS=("default" "dark" "mate")
 
+in_array() {
+    local needle="$1"; shift
+    local el
+    for el in "$@"; do
+        [[ "$el" == "$needle" ]] && return 0
+    done
+    return 1
+}
+
 CONTEXTS+=("${OPTIONAL_CONTEXTS[@]}")
 SIZES+=("${OPTIONAL_SIZES[@]}")
 
 if [ -n "$_variant" ]; then
-	if [[ ! " ${VARIANTS[*]} " =~ " ${_variant} " ]]; then
-		echo "WARNING: Requested $_variant is not known"
-	fi
+    if ! in_array "$_variant" "${VARIANTS[@]}"; then
+        echo "WARNING: Requested \"$_variant\" is not known"
+    fi
 
-	VARIANTS=($_variant)
+    VARIANTS=("$_variant")
 fi
 
 
-linker () {
-	local icon_folder=$1
-	local icon_subfolder=$2
-	declare -A generated_links
+linker() {
+    local icon_folder="$1"
+    local icon_subfolder="$2"
+    declare -A generated_links
 
-	LIST="$DIR/${icon_folder}/$CONTEXT.list"
-	if [ ! -d "$DIR/../../$THEME/$icon_subfolder/$CONTEXT" ]; then
-		dlog "  -- no $icon_subfolder/$CONTEXT, skipping it"
-		return
-	fi
+    LIST="$DIR/${icon_folder}/${CONTEXT}.list"
+    if [ ! -d "$DIR/../../$THEME/$icon_subfolder/$CONTEXT" ]; then
+        dlog "  -- no $icon_subfolder/$CONTEXT, skipping it"
+        return
+    fi
 
-	cd $DIR/../../$THEME/$icon_subfolder/$CONTEXT
-	while read line;
-	do
-		if [[ "$line" =~ ^[[:space:]]*$ ]]; then
-			dlog "Ignoring empty line in $LIST"
-			continue
-		fi
+    cd "$DIR/../../$THEME/$icon_subfolder/$CONTEXT" || return 1
+    while IFS= read -r line;
+    do
+        if [[ "$line" =~ ^[[:space:]]*$ ]]; then
+            dlog "Ignoring empty line in $LIST"
+            continue
+        fi
 
-		if [[ $line != *"$needle"* ]]; then
-			dlog "line $line does not match with $needle: skipping"
-			continue
-		fi
+        if [[ "$line" != *"$needle"* ]]; then
+            dlog "line \"$line\" does not match with \"$needle\": skipping"
+            continue
+        fi
 
-		SOURCE_FILE=${line%% *}
-		if [ -f "$SOURCE_FILE" ]; then
-			line_array=($line)
-			target="${line_array[0]}"
-			link_name="${line_array[1]}"
-			echo "[$icon_subfolder/$CONTEXT] linking $link_name -> $target"
-			if [ -n "${generated_links["$link_name"]}" ]; then
-				echo "  ERROR: $link_name is already linked to ${generated_links["$link_name"]}"
-				exit 1
-			fi
-			if [ "$target" = "$link_name" ]; then
-				echo "  ERROR: Can't link a file with itself!"
-				exit 1
-			fi
-			if [ -L "$target" ]; then
-				echo "  ERROR: $target is already a symlink, please point it to a real file!"
-				exit 1
-			fi
-			if [ -z "$_dry_run" ]; then
-				ln -sf "$target" "$link_name"
-			fi
-			generated_links["$link_name"]="$target"
-		elif [ $VARIANT = "default" ] &&
-			 [[ ! " ${OPTIONAL_SIZES[*]} " =~ " $icon_subfolder " ]] &&
-			 [[ ! " ${OPTIONAL_CONTEXTS[*]} " =~ " $CONTEXT " ]]; then
-			# The default variant must have all icons availables
-			echo "  ERROR: could not find symlink file \"$SOURCE_FILE\" in $(pwd)"
-			exit 1
-		else
-			# The variants can ignore the missing icons
-			dlog "skipping \"$line\" for $VARIANT variant: could not find source symlink file \"$SOURCE_FILE\" in $(pwd)"
-		fi
+        SOURCE_FILE="${line%% *}"
+        if [ -f "$SOURCE_FILE" ]; then
+            read -r -a line_array <<< "$line"
+            target="${line_array[0]}"
+            link_name="${line_array[1]}"
+            echo "[$icon_subfolder/$CONTEXT] linking $link_name -> $target"
+            if [ -n "${generated_links["$link_name"]}" ]; then
+                echo "  ERROR: \"$link_name\" is already linked to \"${generated_links["$link_name"]}\""
+                exit 1
+            fi
+            if [ "$target" = "$link_name" ]; then
+                echo "  ERROR: Can't link a file with itself!"
+                exit 1
+            fi
+            if [ -L "$target" ]; then
+                echo "  ERROR: \"$target\" is already a symlink, please point it to a real file!"
+                exit 1
+            fi
+            if [ -z "$_dry_run" ]; then
+                ln -sf "$target" "$link_name"
+            fi
+            generated_links["$link_name"]="$target"
+        elif [ "$VARIANT" = "default" ] &&
+             ! in_array "$icon_subfolder" "${OPTIONAL_SIZES[@]}" &&
+             ! in_array "$CONTEXT" "${OPTIONAL_CONTEXTS[@]}"; then
+            # The default variant must have all icons available
+            echo "  ERROR: could not find symlink file \"$SOURCE_FILE\" in \"$(pwd)\""
+            exit 1
+        else
+            # The variants can ignore the missing icons
+            dlog "skipping \"$line\" for \"$VARIANT\" variant: could not find source symlink file \"$SOURCE_FILE\" in \"$(pwd)\""
+        fi
 
-	done < $LIST
-	cd $DIR/../../$THEME
-	return $symlinks_counter
+    done < "$LIST"
+    cd "$DIR/../../$THEME" || return 1
 }
 
 # Fullcolor icons
 echo "Generating links for fullcolor icons..."
 for VARIANT in "${VARIANTS[@]}"
 do
-	[[ $VARIANT = "default" ]] && THEME="Yaru" || THEME="Yaru-${VARIANT}"
-	for CONTEXT in "${CONTEXTS[@]}"
-	do
-		dlog " -- "${CONTEXT}
-		for SIZE in "${SIZES[@]}"
-		do
-			linker "fullcolor" $SIZE
-		done
-	done
+    [[ $VARIANT = "default" ]] && THEME="Yaru" || THEME="Yaru-${VARIANT}"
+    for CONTEXT in "${CONTEXTS[@]}"
+    do
+        dlog " -- ${CONTEXT}"
+        for SIZE in "${SIZES[@]}"
+        do
+            linker "fullcolor" "$SIZE"
+        done
+    done
 done
 echo "Done"
 
@@ -213,18 +221,18 @@ echo "Done"
 echo "Generating links for symbolic icons..."
 for VARIANT in "${VARIANTS[@]}"
 do
-	[[ $VARIANT = "default" ]] && THEME="Yaru" || THEME="Yaru-${VARIANT}"
-	for CONTEXT in "${CONTEXTS[@]}"
-	do
-		dlog " -- "$CONTEXT
-		linker "symbolic" "scalable"
-	done
+    [[ $VARIANT = "default" ]] && THEME="Yaru" || THEME="Yaru-${VARIANT}"
+    for CONTEXT in "${CONTEXTS[@]}"
+    do
+        dlog " -- ${CONTEXT}"
+        linker "symbolic" "scalable"
+    done
 done
 echo "Done"
 
 # Clear symlink errors
-if command -v symlinks 2>&1 >/dev/null; then
-	echo "Deleting broken links..."
-	symlinks -cdr $DIR/../../$THEME
-	echo "Done."
+if command -v symlinks &>/dev/null; then
+    echo "Deleting broken links..."
+    symlinks -cdr "$DIR/../../$THEME"
+    echo "Done."
 fi
